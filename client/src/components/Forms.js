797d6d2a1af9c90f3ca7694a917c06243d8b37e6
Lucas Kraus <lucas-kraus@hotmail.com>
2023-09-21 20:54:30 -0300

    Update Forms.js
    Implementação inicial com Axios para envio do feedback para o servidor.

--- client/src/components/Forms.js
+++ client/src/components/Forms.js
@@ -1,10 +1,28 @@
 import './Components.scss';
 import Axios from 'axios';
+import { useState } from 'react';
+
 
 const Forms = () => {
-
+    
     //State para enviar o feedback.
     const [query, setQuery] = useState(false);
+    const [feedback, setFeedback] = useState("");
+
+    useEffect(() => {
+        if (query) {
+            Axios.post("http://localhost:8080/authentication", {
+            feedback: feedback,
+            }).then((response) => {
+                if (response.status === 200 && response.data.length > 0){
+                };
+            }).catch(function(error){
+                console.log(error);
+            });
+        };
+    }, [query] )
+
+    
 }
 
 export default Forms;
