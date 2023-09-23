19ce7069282864f29f3e2e275d0aedfeb88db36d
Marcus Davanço <md.daves@gmail.com>
2023-09-22 22:22:14 -0300

    chore: further styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,35 @@ import './App.scss';
 import { Card } from './components/Card';
 
 function App() {
+  const cards = [
+    {
+      id: 1,
+      type: "Elogio",
+      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut semper urna. Quisque euismod arcu vel augue dictum, ut commodo nunc lacinia. Integer ullamcorper dui sit amet purus.",
+      status: "Recebido"
+    },
+    {
+      id: 2,
+      type: "Sugestão",
+      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut semper urna. Quisque euismod arcu vel augue dictum, ut commodo nunc lacinia. Integer ullamcorper dui sit amet purus.",
+      status: "Em processamento"
+    },
+    {
+      id: 3,
+      type: "Crítica",
+      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut semper urna. Quisque euismod arcu vel augue dictum, ut commodo nunc lacinia. Integer ullamcorper dui sit amet purus.",
+      status: "Finalizado"
+    },
+  ]
+
   return (
     <div className="App">
       <h1>Hello, World!</h1>
       <div className="cardContainer">
-      <Card type={"Elogio"} message={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut semper urna. Quisque euismod arcu vel augue dictum, ut commodo nunc lacinia. Integer ullamcorper dui sit amet purus."}/>
-      <Card type={"Sugestão"} message={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut semper urna. Quisque euismod arcu vel augue dictum, ut commodo nunc lacinia. Integer ullamcorper dui sit amet purus."}/>
-      <Card type={"Crítica"} message={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut semper urna. Quisque euismod arcu vel augue dictum, ut commodo nunc lacinia. Integer ullamcorper dui sit amet purus."}/>
+      {
+        cards.map(card => <Card key={card.id} content={card}/>)
+      }
+      
       </div>
     </div>
   );
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,18 +1,20 @@
 import './Components.scss';
 import PropTypes from 'prop-types';
-export function Card({id, type, message, status}){
+export function Card({content}){
   return (
     <article className="card">
-      <p className={`card__type--elogio card__type card__type--${type}`}>{type}</p>
-      <p className="card__content">{message}</p>
-      <p className="card__status">{status}</p>
+      <p className={`card__type--elogio card__type card__type--${content.type}`}>{content.type}</p>
+      <p className="card__content">{content.message}</p>
+      <p className="card__status">{content.status}</p>
     </article>
   )
 }
 
 Card.propTypes = {
-  id: PropTypes.number,
-  type: PropTypes.oneOf(['Sugestão', 'Elogio', 'Crítica']),
-  message: PropTypes.string,
-  status: PropTypes.oneOf(['Recebido', 'Em Processamento', 'Finalizado']),  
+  content: PropTypes.shape({
+    id: PropTypes.number,
+    type: PropTypes.oneOf(['Sugestão', 'Elogio', 'Crítica']),
+    message: PropTypes.string,
+    status: PropTypes.oneOf(['Recebido', 'Em Processamento', 'Finalizado']),  
+  })  
 }
