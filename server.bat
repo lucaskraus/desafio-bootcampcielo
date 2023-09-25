e3f36c8c6571ed4d05d1fb7a60e675d37e3f6422
Lucas Kraus <lucas-kraus@hotmail.com>
2023-09-24 22:03:24 -0300

    Scripts .bat update

--- server.bat
+++ server.bat
@@ -1,12 +1,4 @@
 @echo off
-<<<<<<< Updated upstream
 cd server\target 
 java -jar primeiro-desafio-0.0.1-SNAPSHOT.jar
-=======
-
-cd C:\Users\Lucas\Desktop\
-
-java -jar servidor.jar
-
->>>>>>> Stashed changes
 pause
